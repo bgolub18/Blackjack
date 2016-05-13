2f30024266d72c9ea45ac4704bec8b2a6882f468
Ben Golub <bgolub@lsoc.org>
2016-05-13 12:06:17 -0500

    pseudo

---
+++ Models/blackjack.rb
@@ -0,0 +1,15 @@
+class Game
+	def intialize
+		@cards = []
+	end
+	#Here's our plan:
+	#Assign values to each card. Give options Hit, Stand, or Quit. Show one of the dealers cards
+	#We might not actually include the dealer because that could be complicated
+	#While playerScore != 21 do (for hitting)
+	#We could also have a default of about 5 people at the table for simplicity
+	#We probably can't do a split yet because that's hard
+	#For HTML we could make each suit either red or black
+	#We could maybe make like a repeat/loop command (I don't remember what it's called)
+	#It will probably be a bunch of if or case statements
+
+		
