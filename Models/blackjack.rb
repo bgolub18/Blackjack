a676e9332b7e0e81a7b53f28ab6356c7d58c2346
Ben Golub <bgolub@lsoc.org>
2016-05-14 16:24:03 -0500

    more stuff

--- Models/blackjack.rb
+++ Models/blackjack.rb
@@ -8,6 +8,8 @@ class Game
 		@deck = (1..52).to_a.shuffle
 		@your_hand = [@deck.shift, @deck.shift]
 		@dealer_hand = [@deck.shift, @deck.shift]
+		@score = (@your_hand[0]%13) + (@your_hand[1]%13)
+		@dealer_score = (@dealer_hand[0]%13) + (@dealer_hand[1]%13)
 	end
 	def card=(card)
 		case (card-1)/13
@@ -39,6 +41,7 @@ class Game
 		case choice
 		when "hit"
 			@your_hand << @deck.shift
+			@score = @score + (your_hand[2]%13)
 			if @score > 21 
 				return "bust"
 				if @dealer_score > 21
@@ -50,22 +53,15 @@ class Game
 				return @score
 			end 	
 		when "stand"
-			return #score
+			return @score
 			if @dealer_score <= @score && @dealer_score <= 16 
 				@dealer_hand << @deck.shift
+				@dealer_score = @dealer_score + (@dealer_hand[2]%13)
 		end
 	end
-
-
-
-
-
-
-
-
 end
 	#Here's our plan:
-	#Assign values to each card. Give options Hit or Stand Show one of the dealers cards
+	#Assign values to each card. Give options Hit or Stand. Show one of the dealers cards
 	#We might not actually include the dealer because that could be complicated
 	#While playerScore != 21 do (for hitting)
 	#We could also have a default of about 5 people at the table for simplicity
