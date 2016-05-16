e3d35d19ca076b4b04c3d7beef4cd4a89f350d73
Ben Golub <bgolub@lsoc.org>
2016-05-16 11:12:14 -0500

    IT WORKS BABAYYYY

--- Models/blackjack.rb
+++ Models/blackjack.rb
@@ -1,15 +1,15 @@
 class Game
-	def intialize(name)
+	def initialize(name)
 		@name = name
 		@players =["Ben", "Reis", "James", "Dean", "Daniel", "Ned"]
-		if @players.include?(@name)
-			@players.delete(@name)
-		end
+		# if @players.include?(@name)
+		# 	@players.delete(@name)
+		# end
 		@deck = (1..52).to_a.shuffle
 		@your_hand = [@deck.shift, @deck.shift]
 		@dealer_hand = [@deck.shift, @deck.shift]
-		@score = (@your_hand[0]%13) + (@your_hand[1]%13)
-		@dealer_score = (@dealer_hand[0]%13) + (@dealer_hand[1]%13)
+		@score = ((@your_hand[0]-1)/4) + ((@your_hand[1]-1)/4)
+		@dealer_score = ((@dealer_hand[0]-1)/4) + ((@dealer_hand[1]-1)/4)
 	end
 	def card=(card)
 		case (card-1)/13
@@ -22,7 +22,7 @@ class Game
 		when 3
 			suit = "clubs"
 		end
-		value = card%13
+		value = (card-1)/4
 		case value
 		when 1
 			face = "Ace"
@@ -41,9 +41,9 @@ class Game
 		case choice
 		when "hit"
 			@your_hand << @deck.shift
-			@score = @score + (your_hand[2]%13)
+			@score = @score + ((@your_hand[2]-1)/4)
 			if @score > 21 
-				return "bust"
+				return "You bust"
 				if @dealer_score > 21
 					return "dealer busts"
 				elsif @dealer_score<= 21
@@ -53,21 +53,22 @@ class Game
 				return @score
 			end 	
 		when "stand"
-			return @score
-			if @dealer_score <= @score && @dealer_score <= 16 
+			while ((@dealer_score <= @score) && (@dealer_score <= 16)) 
 				@dealer_hand << @deck.shift
-				@dealer_score = @dealer_score + (@dealer_hand[2]%13)
+				@dealer_score = @dealer_score + ((@dealer_hand[2]-1)/4)
+			end
 		end
+		if (@dealer_score > 21) && (@score<= 21)
+			return "The dealer busts, and you win." 
+		elsif (@score > 21) && (@dealer_score<= 21)
+			return "You bust, and the dealer wins."
+		elsif (@dealer_score <= 21) && (@score <= 21)
+			if @score > @dealer_score 
+				return "You win!"
+			else 
+				return "The dealer wins."
+			end
+			
+		end	
 	end
 end
-	#Here's our plan:
-	#Assign values to each card. Give options Hit or Stand. Show one of the dealers cards
-	#We might not actually include the dealer because that could be complicated
-	#While playerScore != 21 do (for hitting)
-	#We could also have a default of about 5 people at the table for simplicity
-	#We probably can't do a split yet because that's hard
-	#For HTML we could make each suit either red or black
-	#We could maybe make like a repeat/loop command (I don't remember what it's called)
-	#It will probably be a bunch of if or case statements
-
-		
