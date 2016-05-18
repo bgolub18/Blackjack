941d07e858baa246b7e6d27971a9c0969f981c45
Ben Golub <bgolub@lsoc.org>
2016-05-18 08:29:44 -0500

    stuff

--- Controller.rb
+++ Controller.rb
@@ -1,3 +1,12 @@
 require 'Sinatra'
 require_relative 'Models/blackjack.rb'
 enable :sessions
+
+get '/' do
+	session[:game] = Game.new
+	@hand = "You\'re hand is the #{session[:game].value(@your_hand[0])} and the #{session[:game].value(@your_hand[1])} for a score of #{@score}"
+	@context = "The dealer is showing a #{session[:game].value(@dealer_hand[0])}, do you stand or hit?"
+end
+post '/' do
+	session[:game].play(params[:choice])
+end 
