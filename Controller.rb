ac91fc0c8d696f55fbb71a4ee56f1214a794a0a4
Ben Golub <bgolub@lsoc.org>
2016-05-18 08:58:50 -0500

    building

--- Controller.rb
+++ Controller.rb
@@ -6,7 +6,14 @@ get '/' do
 	session[:game] = Game.new
 	@hand = "You\'re hand is the #{session[:game].value(@your_hand[0])} and the #{session[:game].value(@your_hand[1])} for a score of #{@score}"
 	@context = "The dealer is showing a #{session[:game].value(@dealer_hand[0])}, do you stand or hit?"
+	erb :index
 end
 post '/' do
 	session[:game].play(params[:choice])
+	case params[:choice]
+	when "hit"
+		@hand = "You\'re score is now #{@score}."
+		if @score > 21
+			@end = "You busted. The dealer wins with a score of #{@dealer_score}."
+	
 end 
