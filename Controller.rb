7a1549c43cd0d5690caa897bf2989865410f0b2d
Ben Golub <bgolub@lsoc.org>
2016-05-19 14:59:39 -0500

    Valuing cards = complete

--- Controller.rb
+++ Controller.rb
@@ -4,7 +4,7 @@ enable :sessions
 
 get '/' do
 	session[:game] = Game.new
-	@hand = "You\'re hand is the #{session[:game].value(session[:game].your_hand[0])} and the #{session[:game].value(session[:game].your_hand[1])} for a score of #{session[:game].score}"
+	@hand = "You\'re hand is the #{session[:game].value(session[:game].your_hand[0])} and the #{session[:game].value(session[:game].your_hand[1])} for a score of #{session[:game].your_score}"
 	@context = "The dealer is showing a #{session[:game].value(session[:game].dealer_hand[0])}, do you stand or hit?"
 	erb :index
 end
@@ -12,8 +12,8 @@ post '/' do
 	session[:game].play(params[:choice])
 	case params[:choice]
 	when "hit"
-		@hand = "You\'re score is now #{session[:game].score}."
-		if session[:game].score > 21
+		@hand = "You\'re score is now #{session[:game].your_score}."
+		if session[:game].your_score > 21
 			@end = "You busted. The dealer wins with a score of #{session[:game].dealer_score}."
 			erb :results
 		else
--- Models/blackjack.rb
+++ Models/blackjack.rb
@@ -1,5 +1,5 @@
 class Game
-	attr_accessor :your_hand, :dealer_hand, :score, :dealer_score
+	attr_accessor :your_hand, :dealer_hand, :your_score, :dealer_score
 	def initialize
 		#@name = name
 		#@players =["Ben", "Reis", "James", "Dean", "Daniel", "Ned"]
@@ -9,19 +9,39 @@ class Game
 		@deck = (1..52).to_a.shuffle
 		@your_hand = [@deck.shift, @deck.shift]
 		@dealer_hand = [@deck.shift, @deck.shift]
-		@score = ((@your_hand[0]-1)/4) + ((@your_hand[1]-1)/4))
-		@dealer_score = ((@dealer_hand[0]-1)/4) + ((@dealer_hand[1]-1)/4)
+		@your_score = 0
+		@your_hand.each do |card|
+			@your_score += score(card)
+		end
+		@dealer_score = 0
+		@dealer_hand.each do |card|
+			@dealer_score += score(card)
+		end
 	end
+	def score(card)
+		case (card-1)/4
+		when 0 
+			value = 10
+		when 11
+			value = 10
+		when 12
+			value = 10
+		else
+			value = (card-1)/4
+		end
+		return value
+	end
+
 	def value(card)
 		case (card-1)/13
 		when 0
-			suit = "spades"
+			suit = "Spades"
 		when 1
-			suit = "hearts"
+			suit = "Hearts"
 		when 2
-			suit = "diamonds"
+			suit = "Diamonds"
 		when 3
-			suit = "clubs"
+			suit = "Clubs"
 		end
 		value = (card-1)/4
 		case value
@@ -42,8 +62,11 @@ class Game
 		case choice
 		when "hit"
 			@your_hand << @deck.shift
-			@score = @score + ((@your_hand[2]-1)/4)
-			if @score > 21 
+			@your_score = 0
+			@your_hand.each do |card|
+				@your_score += score(card)
+			end
+			if @your_score > 21 
 				return "You bust"
 				if @dealer_score > 21
 					return "dealer busts"
@@ -52,17 +75,20 @@ class Game
 				end
 			end 	
 		when "stand"
-			while ((@dealer_score <= @score) && (@dealer_score <= 16)) 
+			while ((@dealer_score <= @your_score) && (@dealer_score <= 16)) 
 				@dealer_hand << @deck.shift
-				@dealer_score = @dealer_score + ((@dealer_hand[2]-1)/4)
+				@dealer_score = 0
+				@dealer_hand.each do |card|
+					@dealer_score += score(card)
+				end
 			end
 		end
-		if (@dealer_score > 21) && (@score<= 21)
+		if (@dealer_score > 21) && (@your_score<= 21)
 			return "The dealer busts, and you win." 
-		elsif (@score > 21) && (@dealer_score<= 21)
+		elsif (@your_score > 21) && (@dealer_score<= 21)
 			return "You bust, and the dealer wins."
-		elsif (@dealer_score <= 21) && (@score <= 21)
-			if @score > @dealer_score 
+		elsif (@dealer_score <= 21) && (@your_score <= 21)
+			if @your_score > @dealer_score 
 				return "You win!"
 			else 
 				return "The dealer wins."
